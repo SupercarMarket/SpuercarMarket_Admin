09b34352f2f92d715b9eba133b38e1835786a171
jigomgom <dydrkfl5519@gmail.com>
2022-12-15 18:37:34 +0900

    design: input 기본 속성 주석처리, table 공용 속성 업데이트

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -45,12 +45,13 @@ const GlobalStyle = createGlobalStyle`
 
   /* input 기본 스타일 초기화 */
   input {
-      -webkit-appearance: none;
+      /* -webkit-appearance: none;
         -moz-appearance: none;
               appearance: none;
+            */
       :focus{
         outline: none;
-      }
+      } 
   }
 
   /* list 스타일 */
@@ -62,6 +63,19 @@ const GlobalStyle = createGlobalStyle`
   /* a 태그 reset*/
   a{
     all: unset;
+  }
+
+  /* table 스타일 */
+  table{
+    width: 100%;    
+    border : 1px solid #EAEAEC;
+    border-collapse: collapse;
+  }
+
+  th, td {
+    text-align:center;
+    vertical-align:middle;
+    border : 1px solid #EAEAEC;
   }
 `;
 
