a87e8cd0e4df9c870062d12613cd3def1eeee1bb
jigomgom <dydrkfl5519@gmail.com>
2022-12-16 16:01:18 +0900

    refactor: DropDown component 로직 분리, DropDownDataType 정의 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import LoginForm from './components/Login/LoginForm';
+import HeaderForm from './components/Header/HeaderForm';
+import SideMenuForm from './components/SideMenu/SideMenuForm';
+import MarketForm from './components/Market/MarketForm';
+
+const isLogin = true;
 
 function App() {
   return (
     <>
+      {isLogin ? (
+        <>
+          <HeaderForm />
+          <div style={{ display: "flex" }}>
+            <SideMenuForm />
+            <MarketForm />
+          </div>
+        </>
+      ) : (
+        <>
+          <LoginForm />
+        </>
+      )}
     </>
   );
 }
---
+++ src/hooks/DropDown/useDropDownHooks.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from "react";
+import { DropDownItemMap } from "../../types/DropDownType";
+
+interface useDetectPropsType{
+    initState : boolean;
+    title : string;
+}
+
+export const useDetectOutSideHandler = ( { initState, title } : useDetectPropsType ) => {
+    const [ isOpen , setIsOpen ] = useState< boolean >( initState );
+    const [ isTitle, setIsTitle ] = useState< string >( DropDownItemMap[ title ][0].name );
+    const outSideRef = useRef<HTMLDivElement>(null);
+
+    // dropdown 목록 열기
+    const openDropDownItemHandler = ( isOpen : boolean ) => {
+        setIsOpen( !isOpen );
+    };
+
+    // dropdown item 클릭시 title 바꾸고 목록 닫기
+    const changeDropDownItemTitleHanlder = ( text : string ) => {
+        setIsTitle( text );
+        setIsOpen( false );  
+    };
+
+    // 외부 영역 클릭시, 목록 닫기
+    useEffect(()=>{
+        const onCheckOutSideHandler = ( event : CustomEvent<MouseEvent> ) => {
+          console.log( outSideRef.current );
+          if( isOpen && outSideRef.current && ! outSideRef.current.contains( event.target as Node ) ){
+            setIsOpen( false );
+          }
+        };
+        document.addEventListener('mousedown', onCheckOutSideHandler as EventListener );
+        return () => {
+          document.removeEventListener( "mousedown", onCheckOutSideHandler as EventListener );
+        }
+      },[ outSideRef.current ]);
+    return{
+        isOpen : isOpen,
+        isTitle : isTitle,
+        ref : outSideRef,
+        openDropDownFn : openDropDownItemHandler,
+        changeDropDownTitleFn : changeDropDownItemTitleHanlder
+    }
+};
