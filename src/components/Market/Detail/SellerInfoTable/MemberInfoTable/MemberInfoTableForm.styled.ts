485d553137aa28c36fc12b79c8b88019fdcf14b8
jigomgom <dydrkfl5519@gmail.com>
2022-12-17 17:59:55 +0900

    feat: 매장 관리 -> 매물 리스트 -> 디테일 개발, 공용 관리할 Detail 디렉토리 셍성

---
+++ src/components/Market/Detail/SellerInfoTable/MemberInfoTable/MemberInfoTableForm.styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { SellerInfoHeader, SellerInfoContent } from "../SellerInfoTableForm.styled";
+
+export const MemberInfoWrapper = styled.div`
+    width: 780px;
+    height : 373px;
+`;
+
+export const MemeberTable = styled.table`
+    width: 100%;
+    margin-top : 20px;
+`;
+
+export const MemberHeader = styled( SellerInfoHeader )``;
+
+export const MemberContent= styled( SellerInfoContent )``;
