e26721671736a8b00144ae4e12457a3df6d6e538
jigomgom <dydrkfl5519@gmail.com>
2022-12-22 15:42:52 +0900

    style: type 명 오류 수정

--- src/types/PaginationType.ts
+++ src/types/PaginationType.ts
@@ -2,7 +2,7 @@ import React from "react";
 
 export interface PaginationPropsType {
     // 페이지 수
-    pagintaionCount : number;
+    paginationCount : number;
     // 페이지에 그려지는 개수 default 20개씩
     postsPerPage : number;
     // 이동될 페이지
