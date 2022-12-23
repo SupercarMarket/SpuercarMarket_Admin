59b33fd2589a71c9ca8a5772fdb533600840fe20
jigomgom <dydrkfl5519@gmail.com>
2022-12-23 14:55:52 +0900

    feat: 작은 dropdown 공용 component 생성

---
+++ src/components/Common/SmallDropDown/SmallDropDownForm.styled.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import Arrow from "../../../assets/small_expand_arrow.svg";
+
+export const Wrapper = styled.div`
+    position: relative;
+    width: 80px;
+    height: 32px;
+`;
+
+export const Selecter = styled.button`
+    display: flex;
+    align-items: center;
+    padding: 0px 8px;
+
+    width: 80px;
+    height: 32px;
+
+    background-color: #FFFFFF;
+
+    border: 1px solid #C3C3C7;
+    border-radius: 4px;
+
+    font-weight: ${({theme}) => theme.fontWeight.normal};
+    font-size: ${({theme}) => theme.fontSize.body_14};
+    line-height: 150%;
+    
+    color: ${({theme}) => theme.colors.greyScale_5};
+`;
+
+export const SelecterArrow = styled.img.attrs( { src : `${Arrow}`})`
+    position: absolute;
+
+    top : 12.11px;
+    right : 12.11px;
+`;
+
+export const OptionWrapper = styled.ul<{ isClicked : boolean }>`
+    display: ${({isClicked}) => isClicked ? "block": "none"};
+    
+    width: 80px;
+    
+    background: #FFFFFF;
+    border: 1px solid ${({theme}) => theme.colors.greyScale_3};
+    border-radius: 4px;
+`;
+
+export const OptionItem = styled.li`
+    width: 80px;
+    height: 32px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0px 8px;
+
+
+    &:hover{
+        background: ${({theme}) => theme.colors.greyScale_2};
+        color: ${({theme}) => theme.colors.primary};
+        font-weight: ${({theme}) => theme.fontWeight.bold };
+    }
+
+    cursor: pointer;
+`;
