a7cd6902f08f7e90ac5759c593ccbb7caf5bc71a
jigomgom <dydrkfl5519@gmail.com>
2023-02-17 20:40:14 +0900

    remove : 파일 지우기

--- src/components/Advertisement/AdvertisementInquiry/AdvertisementInquiryForm.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdvertisementInquiryForm = () => {
-  return (
-    <div>AdvertisementInquiryForm</div>
-  )
-}
-
-export default AdvertisementInquiryForm
--- src/components/Advertisement/AdvertisementList/Information/AdvertisementInfoForm.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdvertisementInfoForm = () => {
-  return (
-    <div>AdvertisementInfoForm</div>
-  )
-}
-
-export default AdvertisementInfoForm
