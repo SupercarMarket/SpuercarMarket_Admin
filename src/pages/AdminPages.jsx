65abed1634f273b803122f81b676d77ac3275ccb
jigomgom <dydrkfl5519@gmail.com>
2023-02-20 16:26:02 +0900

    conflict: conflict 수정

--- src/pages/AdminPages.jsx
+++ src/pages/AdminPages.jsx
@@ -7,15 +7,6 @@ import LoginForm from "../components/Login/LoginForm";
 const AdminPages = () => {
   return (
     <Routes>
-        <Route element={<PrivateRoutes />}>
-            <Route path="/salelist" element={<ForSaleListForm />}>
-                <Route path=":saleId" element={<ForSaleListDetailForm/>} />
-            </Route>
-            <Route path='/saleinquriy' element={<VehicleRegistrationInquriyForm/>} >
-                <Route path=":inquriyId" element={<VehicleRegistrationInquiryDetailForm/>} />
-            </Route>
-        </Route>
-
         {/* Login 아니면 */}
         <Route path="/" element={<LoginForm />} />
     </Routes>
