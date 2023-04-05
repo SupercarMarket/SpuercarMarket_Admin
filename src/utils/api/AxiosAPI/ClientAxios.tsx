a8c2afb0cd78af656bcb4ccc3c1529ac0978e3a5
4802852 <4802852@gmail.com>
2023-04-06 01:14:32 +0900

    feat: 로그인 정보 만료시 로그인 페이지로 자동 이동

--- src/utils/api/AxiosAPI/ClientAxios.tsx
+++ src/utils/api/AxiosAPI/ClientAxios.tsx
@@ -10,53 +10,63 @@ interface Error405Type {
     path?: string;
     status?: number;
     timestamp?: string;
-};
+}
 
 const ClientAxios = axios.create({
-    baseURL: `${URL}`
+    baseURL: `${URL}`,
 });
 
 //interceptor request
-ClientAxios.interceptors.request.use( async ( config ) => {
+ClientAxios.interceptors.request.use(async (config) => {
     const refreshToken = getCookie(LoginType.refresh as string);
     const accessToken = getCookie(LoginType.access as string);
     // refresh token, access token save
-    if( refreshToken && accessToken ){
+    if (refreshToken && accessToken) {
         config.headers.ACCESS_TOKEN = accessToken;
         config.headers.REFRESH_TOKEN = refreshToken;
     }
 
-    if( refreshToken ){
-        try{
-            await axios.post(`${URL}user/get-token`, {},
-            {
-                headers:{
-                    ACCESS_TOKEN: accessToken,
-                    REFRESH_TOKEN : refreshToken
+    if (refreshToken) {
+        try {
+            await axios.post(
+                `${URL}user/get-token`,
+                {},
+                {
+                    headers: {
+                        ACCESS_TOKEN: accessToken,
+                        REFRESH_TOKEN: refreshToken,
+                    },
                 }
-            });
-        }catch( error ){
-          const { response } = error as unknown as AxiosError;
-          if (response) {
-            /*
+            );
+        } catch (error) {
+            const { response } = error as unknown as AxiosError;
+            if (response) {
+                /*
                 response.status = 405, Method Not Allow
                 response.status = 501, access token refresh
             */
-            if (response.status === 425 ) {
-              setCookie( LoginType.access as string, response.headers.access_token, { path:"/", secure: true});
-              config.headers.ACCESS_TOKEN = response.headers.access_token;
-            } else {
-              console.log((response.data as unknown as Error405Type).error);
-              if( response.status === 422 ){
-                alert('로그인 사용 정보가 만료되었습니다. 재 로그인해주세요.');
-                removeCookie( LoginType.refresh as string, { path : "/" } );
-                removeCookie( LoginType.access as string, { path : "/" } );
-              }
+                if (response.status === 425) {
+                    setCookie(LoginType.access as string, response.headers.access_token, { path: "/", secure: true });
+                    config.headers.ACCESS_TOKEN = response.headers.access_token;
+                } else {
+                    console.log((response.data as unknown as Error405Type).error);
+                    const navigate = useNavigate();
+                    if (response.status === 422) {
+                        alert("로그인 사용 정보가 만료되었습니다. 재 로그인해주세요.");
+                        removeCookie(LoginType.refresh as string, { path: "/" });
+                        removeCookie(LoginType.access as string, { path: "/" });
+                        navigate("/");
+                    } else if (response.status === 420) {
+                        alert("로그인 사용 정보가 만료되었습니다. 재 로그인해주세요.");
+                        removeCookie(LoginType.refresh as string, { path: "/" });
+                        removeCookie(LoginType.access as string, { path: "/" });
+                        navigate("/");
+                    }
+                }
             }
-          }
         }
     }
     return config;
-})
+});
 
 export default ClientAxios;
