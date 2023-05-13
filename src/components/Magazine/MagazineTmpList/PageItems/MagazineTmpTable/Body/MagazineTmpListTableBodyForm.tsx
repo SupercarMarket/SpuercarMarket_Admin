b6f5446f8410065f914c83560bc8a80d7a20f40d
doyupKim <gltlvl12@gmail.com>
2023-05-13 15:37:50 +0900

    feat. 리스트 페이징 처리 수정(slice 제거), 비밀번호 초기화 endpoint(body값) 수정

--- src/components/Magazine/MagazineTmpList/PageItems/MagazineTmpTable/Body/MagazineTmpListTableBodyForm.tsx
+++ src/components/Magazine/MagazineTmpList/PageItems/MagazineTmpTable/Body/MagazineTmpListTableBodyForm.tsx
@@ -1,11 +1,11 @@
 import React, { useRef } from "react";
 import {
-    MagazineListTableBody,
-    MagazineListTableBodyRowSpan,
-    MagazineListCheckBoxWrapper,
-    MagazineListInputCheckBox,
-    MagazineLiseLabelCheckBox,
-    MagazineListTableBodyButton,
+  MagazineListTableBody,
+  MagazineListTableBodyRowSpan,
+  MagazineListCheckBoxWrapper,
+  MagazineListInputCheckBox,
+  MagazineLiseLabelCheckBox,
+  MagazineListTableBodyButton,
 } from "./MagazineTmpListTableBodyForm.styled";
 
 import { MagazineTmpAction } from "redux/modules/MagazineTmpSlice";
@@ -14,66 +14,92 @@ import { useNavigate } from "react-router";
 
 import { MagazineListTableProps } from "../MagazineTmpListTableForm";
 
-const MagazineTmpListTableBodyForm = ({ offset, postsPerPage, totalContentsCount, deleteButtonOnClickHandler }: MagazineListTableProps) => {
-    const inputCheckTypeRef = useRef<HTMLInputElement>(null);
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-    const { list, checkList } = useAppSelector((state) => state.MagazineTmpSlice);
+const MagazineTmpListTableBodyForm = ({
+  offset,
+  postsPerPage,
+  totalContentsCount,
+  deleteButtonOnClickHandler,
+}: MagazineListTableProps) => {
+  const inputCheckTypeRef = useRef<HTMLInputElement>(null);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { list, checkList } = useAppSelector((state) => state.MagazineTmpSlice);
 
-    const inputCheckOnClickHandler = () => {
-        console.log(inputCheckTypeRef.current?.checked);
-    };
+  const inputCheckOnClickHandler = () => {
+    console.log(inputCheckTypeRef.current?.checked);
+  };
 
-    // 항목 체크 박스 셋업
-    const magazineCheckBoxClickHandler = (brdSeq: number, isChecked: boolean) => {
-        dispatch(MagazineTmpAction.setMagazineListEachChecked({ brdSeq, isChecked }));
-    };
-
-    const inputCheckOnChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        magazineCheckBoxClickHandler(parseInt(event.target.id), event.target.checked);
-    };
-
-    // 매거진 디테일로 넘어가기
-    const magazineModifyOnClickHandler = (id: number) => {
-        navigate(`/magazinetmp/editor`, { state: { id: id } });
-    };
-
-    return (
-        <MagazineListTableBody key={`uuid`}>
-            {list.slice(offset, offset + postsPerPage).map((magazine, index) => {
-                return (
-                    <React.Fragment key={magazine.id}>
-                        <tr>
-                            <MagazineListTableBodyRowSpan>
-                                <MagazineListCheckBoxWrapper>
-                                    <MagazineListInputCheckBox
-                                        id={magazine.id.toString()}
-                                        ref={inputCheckTypeRef}
-                                        onClick={inputCheckOnClickHandler}
-                                        onChange={(event) => {
-                                            inputCheckOnChangeHandler(event);
-                                        }}
-                                        checked={checkList.includes(magazine.id) ? true : false}
-                                        disabled={false}
-                                    />
-                                    <MagazineLiseLabelCheckBox htmlFor={magazine.id.toString()} />
-                                </MagazineListCheckBoxWrapper>
-                            </MagazineListTableBodyRowSpan>
-                            <MagazineListTableBodyRowSpan>{String(magazine.id.toString()).padStart(7, "0")}</MagazineListTableBodyRowSpan>
-                            <MagazineListTableBodyRowSpan>{magazine.title}</MagazineListTableBodyRowSpan>
-                            <MagazineListTableBodyRowSpan>{magazine.created}</MagazineListTableBodyRowSpan>
-                            <MagazineListTableBodyRowSpan>
-                                <MagazineListTableBodyButton onClick={() => magazineModifyOnClickHandler(magazine.id)}>수정하기</MagazineListTableBodyButton>
-                            </MagazineListTableBodyRowSpan>
-                            <MagazineListTableBodyRowSpan>
-                                <MagazineListTableBodyButton onClick={() => deleteButtonOnClickHandler([magazine.id])}>삭제하기</MagazineListTableBodyButton>
-                            </MagazineListTableBodyRowSpan>
-                        </tr>
-                    </React.Fragment>
-                );
-            })}
-        </MagazineListTableBody>
+  // 항목 체크 박스 셋업
+  const magazineCheckBoxClickHandler = (brdSeq: number, isChecked: boolean) => {
+    dispatch(
+      MagazineTmpAction.setMagazineListEachChecked({ brdSeq, isChecked })
     );
+  };
+
+  const inputCheckOnChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    magazineCheckBoxClickHandler(
+      parseInt(event.target.id),
+      event.target.checked
+    );
+  };
+
+  // 매거진 디테일로 넘어가기
+  const magazineModifyOnClickHandler = (id: number) => {
+    navigate(`/magazinetmp/editor`, { state: { id: id } });
+  };
+
+  return (
+    <MagazineListTableBody key={`uuid`}>
+      {list.map((magazine, index) => {
+        return (
+          <React.Fragment key={magazine.id}>
+            <tr>
+              <MagazineListTableBodyRowSpan>
+                <MagazineListCheckBoxWrapper>
+                  <MagazineListInputCheckBox
+                    id={magazine.id.toString()}
+                    ref={inputCheckTypeRef}
+                    onClick={inputCheckOnClickHandler}
+                    onChange={(event) => {
+                      inputCheckOnChangeHandler(event);
+                    }}
+                    checked={checkList.includes(magazine.id) ? true : false}
+                    disabled={false}
+                  />
+                  <MagazineLiseLabelCheckBox htmlFor={magazine.id.toString()} />
+                </MagazineListCheckBoxWrapper>
+              </MagazineListTableBodyRowSpan>
+              <MagazineListTableBodyRowSpan>
+                {String(magazine.id.toString()).padStart(7, "0")}
+              </MagazineListTableBodyRowSpan>
+              <MagazineListTableBodyRowSpan>
+                {magazine.title}
+              </MagazineListTableBodyRowSpan>
+              <MagazineListTableBodyRowSpan>
+                {magazine.created}
+              </MagazineListTableBodyRowSpan>
+              <MagazineListTableBodyRowSpan>
+                <MagazineListTableBodyButton
+                  onClick={() => magazineModifyOnClickHandler(magazine.id)}
+                >
+                  수정하기
+                </MagazineListTableBodyButton>
+              </MagazineListTableBodyRowSpan>
+              <MagazineListTableBodyRowSpan>
+                <MagazineListTableBodyButton
+                  onClick={() => deleteButtonOnClickHandler([magazine.id])}
+                >
+                  삭제하기
+                </MagazineListTableBodyButton>
+              </MagazineListTableBodyRowSpan>
+            </tr>
+          </React.Fragment>
+        );
+      })}
+    </MagazineListTableBody>
+  );
 };
 
 export default MagazineTmpListTableBodyForm;
--- src/components/Member/MemberList/PageItems/MemberTable/Body/MemberTableBodyForm.tsx
+++ src/components/Member/MemberList/PageItems/MemberTable/Body/MemberTableBodyForm.tsx
@@ -1,5 +1,12 @@
 import React, { useRef } from "react";
-import { MemberTableBody, MemberTableBodyRowSpan, MemberCheckBoxWrapper, MemberInputCheckBox, MemberLabelCheckBox, MemberTableBodyButton } from "./MemberTableBodyForm.styled";
+import {
+  MemberTableBody,
+  MemberTableBodyRowSpan,
+  MemberCheckBoxWrapper,
+  MemberInputCheckBox,
+  MemberLabelCheckBox,
+  MemberTableBodyButton,
+} from "./MemberTableBodyForm.styled";
 
 import { MemberAction } from "redux/modules/MemberSlice";
 import { useAppDispatch, useAppSelector } from "store/rootReducer";
@@ -8,99 +15,143 @@ import { useNavigate } from "react-router";
 import { MemberTableProps } from "../MemberTable";
 
 const classOptions: { [key: string]: string } = {
-    "1": "브론즈",
-    "2": "실버",
-    "3": "골드",
-    "4": "플레티넘",
-    "5": "다이아",
+  "1": "브론즈",
+  "2": "실버",
+  "3": "골드",
+  "4": "플레티넘",
+  "5": "다이아",
 };
 
-const MemberTableBodyForm = ({ offset, postsPerPage, totalContentsCount, banMemberHandler, unbanMemberHandler }: MemberTableProps) => {
-    const inputCheckTypeRef = useRef<HTMLInputElement>(null);
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-    const { list, checkList } = useAppSelector((state) => state.MemberSlice);
+const MemberTableBodyForm = ({
+  offset,
+  postsPerPage,
+  totalContentsCount,
+  banMemberHandler,
+  unbanMemberHandler,
+}: MemberTableProps) => {
+  const inputCheckTypeRef = useRef<HTMLInputElement>(null);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { list, checkList } = useAppSelector((state) => state.MemberSlice);
 
-    const inputCheckOnClickHandler = () => {
-        console.log(inputCheckTypeRef.current?.checked);
-    };
+  const inputCheckOnClickHandler = () => {
+    console.log(inputCheckTypeRef.current?.checked);
+  };
 
-    // 항목 체크 박스 셋업
-    const userCheckBoxClickHandler = (userSeq: number, isChecked: boolean) => {
-        dispatch(MemberAction.setMemberListEachChecked({ userSeq, isChecked }));
-    };
+  // 항목 체크 박스 셋업
+  const userCheckBoxClickHandler = (userSeq: number, isChecked: boolean) => {
+    dispatch(MemberAction.setMemberListEachChecked({ userSeq, isChecked }));
+  };
 
-    const inputCheckOnChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        userCheckBoxClickHandler(parseInt(event.target.id), event.target.checked);
-    };
+  const inputCheckOnChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    userCheckBoxClickHandler(parseInt(event.target.id), event.target.checked);
+  };
 
-    // 매물 디테일로 넘어가기
-    const memberDetailOnClickHandler = (userSeq: number) => {
-        navigate(`/memberlist/${userSeq.toString()}`);
-    };
+  // 매물 디테일로 넘어가기
+  const memberDetailOnClickHandler = (userSeq: number) => {
+    navigate(`/memberlist/${userSeq.toString()}`);
+  };
 
-    return (
-        <MemberTableBody key={`uuid`}>
-            {list.slice(offset, offset + postsPerPage).map((user, index) => {
-                return (
-                    <React.Fragment key={user.userSeq}>
-                        <tr>
-                            <MemberTableBodyRowSpan rowSpan={2}>
-                                <MemberCheckBoxWrapper>
-                                    <MemberInputCheckBox
-                                        id={user.userSeq.toString()}
-                                        ref={inputCheckTypeRef}
-                                        onClick={inputCheckOnClickHandler}
-                                        onChange={(event) => {
-                                            inputCheckOnChangeHandler(event);
-                                        }}
-                                        checked={checkList.includes(user.userSeq) ? true : false}
-                                        disabled={user.isBanned}
-                                    />
-                                    <MemberLabelCheckBox htmlFor={user.userSeq.toString()} />
-                                </MemberCheckBoxWrapper>
-                            </MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan rowSpan={2}>{String(user.userSeq.toString()).padStart(7, "0")}</MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan colSpan={2}>{user.userId}</MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan>{user.phone}</MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan rowSpan={2}>{user.signUpdate.split("T")[0]}</MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan rowSpan={2}>{classOptions[user.userRating]}</MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan rowSpan={2}>
-                                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "10px" }}>
-                                        {user.isDealer === true ? (
-                                            <>
-                                                딜러
-                                                <MemberTableBodyButton id={user.userSeq.toString()} onClick={() => memberDetailOnClickHandler(user.userSeq)}>
-                                                    정보
-                                                </MemberTableBodyButton>
-                                            </>
-                                        ) : (
-                                            <>일반</>
-                                        )}
-                                    </div>
-                                </div>
-                            </MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan>{user.postNumber}</MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan rowSpan={2}>
-                                {user.isBanned ? (
-                                    <MemberTableBodyButton onClick={() => unbanMemberHandler(user.userSeq)}>차단 풀기</MemberTableBodyButton>
-                                ) : (
-                                    <MemberTableBodyButton onClick={() => banMemberHandler(user.userSeq)}>회원 차단</MemberTableBodyButton>
-                                )}
-                            </MemberTableBodyRowSpan>
-                        </tr>
-                        <tr>
-                            <MemberTableBodyRowSpan>{user.userName}</MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan>{user.nickname}</MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan>{user.email}</MemberTableBodyRowSpan>
-                            <MemberTableBodyRowSpan>{user.commentNumber}</MemberTableBodyRowSpan>
-                        </tr>
-                    </React.Fragment>
-                );
-            })}
-        </MemberTableBody>
-    );
+  return (
+    <MemberTableBody key={`uuid`}>
+      {list.map((user, index) => {
+        return (
+          <React.Fragment key={user.userSeq}>
+            <tr>
+              <MemberTableBodyRowSpan rowSpan={2}>
+                <MemberCheckBoxWrapper>
+                  <MemberInputCheckBox
+                    id={user.userSeq.toString()}
+                    ref={inputCheckTypeRef}
+                    onClick={inputCheckOnClickHandler}
+                    onChange={(event) => {
+                      inputCheckOnChangeHandler(event);
+                    }}
+                    checked={checkList.includes(user.userSeq) ? true : false}
+                    disabled={user.isBanned}
+                  />
+                  <MemberLabelCheckBox htmlFor={user.userSeq.toString()} />
+                </MemberCheckBoxWrapper>
+              </MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan rowSpan={2}>
+                {String(user.userSeq.toString()).padStart(7, "0")}
+              </MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan colSpan={2}>
+                {user.userId}
+              </MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan>{user.phone}</MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan rowSpan={2}>
+                {user.signUpdate.split("T")[0]}
+              </MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan rowSpan={2}>
+                {classOptions[user.userRating]}
+              </MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan rowSpan={2}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      gap: "10px",
+                    }}
+                  >
+                    {user.isDealer === true ? (
+                      <>
+                        딜러
+                        <MemberTableBodyButton
+                          id={user.userSeq.toString()}
+                          onClick={() =>
+                            memberDetailOnClickHandler(user.userSeq)
+                          }
+                        >
+                          정보
+                        </MemberTableBodyButton>
+                      </>
+                    ) : (
+                      <>일반</>
+                    )}
+                  </div>
+                </div>
+              </MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan>{user.postNumber}</MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan rowSpan={2}>
+                {user.isBanned ? (
+                  <MemberTableBodyButton
+                    onClick={() => unbanMemberHandler(user.userSeq)}
+                  >
+                    차단 풀기
+                  </MemberTableBodyButton>
+                ) : (
+                  <MemberTableBodyButton
+                    onClick={() => banMemberHandler(user.userSeq)}
+                  >
+                    회원 차단
+                  </MemberTableBodyButton>
+                )}
+              </MemberTableBodyRowSpan>
+            </tr>
+            <tr>
+              <MemberTableBodyRowSpan>{user.userName}</MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan>{user.nickname}</MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan>{user.email}</MemberTableBodyRowSpan>
+              <MemberTableBodyRowSpan>
+                {user.commentNumber}
+              </MemberTableBodyRowSpan>
+            </tr>
+          </React.Fragment>
+        );
+      })}
+    </MemberTableBody>
+  );
 };
 
 export default MemberTableBodyForm;
