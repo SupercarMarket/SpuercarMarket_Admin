2f556b42019867b7f552146b89ff49b7494311e4
doyupKim <gltlvl12@gmail.com>
2023-05-13 15:21:46 +0900

    feat. 어드민 프로필사진 string이 아닌 image 처리

--- src/components/Member/AdminList/PageItems/Table/Body/AdminTableBodyForm.styled.ts
+++ src/components/Member/AdminList/PageItems/Table/Body/AdminTableBodyForm.styled.ts
@@ -2,42 +2,58 @@ import styled from "styled-components";
 import { Button } from "components/Common/Button/ButtonForm.styled";
 
 export const AdminTableBody = styled.tbody`
-    height: 116px;
+  height: 116px;
 
-    font-weight: ${({ theme }) => theme.fontWeight.normal};
-    font-size: ${({ theme }) => theme.fontSize.body_14};
-    line-height: 150%;
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+  font-size: ${({ theme }) => theme.fontSize.body_14};
+  line-height: 100%;
 
-    color: ${({ theme }) => theme.colors.greyScale_6};
+  color: ${({ theme }) => theme.colors.greyScale_6};
 `;
 
 export const AdminTableBodyRowSpan = styled.td`
-    height: 116px;
-    text-align: center;
+  height: 116px;
+  text-align: center;
 
-    text-align: center;
-    vertical-align: middle;
-    border: 1px solid ${({ theme }) => theme.colors.greyScale_3};
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid ${({ theme }) => theme.colors.greyScale_3};
+`;
+
+export const AdminTableProfileImg = styled.img`
+  margin-top: 8px;
+  //margin-bottom: 8px;
+  margin-left: 38.89px;
+  width: 100px;
+  height: 100px;
+
+  background: #d9d9d9;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
 `;
 
 export const AdminTableBodyClamp = styled.div`
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-line-clamp: 3; /* ellipsis line */
-    -webkit-box-orient: vertical;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 3; /* ellipsis line */
+  -webkit-box-orient: vertical;
 `;
 
 export const AdminTableBodyNoSpan = styled.td.attrs({})`
-    text-align: center;
-    vertical-align: middle;
-    border: 1px solid ${({ theme }) => theme.colors.greyScale_3};
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid ${({ theme }) => theme.colors.greyScale_3};
 `;
 
 export const AdminTableBodyButton = styled(Button)`
-    background-color: ${({ theme }) => theme.colors.greyScale_1};
-    color: ${({ theme }) => theme.colors.greyScale_6};
+  background-color: ${({ theme }) => theme.colors.greyScale_1};
+  color: ${({ theme }) => theme.colors.greyScale_6};
 
-    text-align: center;
-    vertical-align: middle;
-    border: 1px solid ${({ theme }) => theme.colors.greyScale_4};
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid ${({ theme }) => theme.colors.greyScale_4};
 `;
--- src/components/Member/AdminList/PageItems/Table/Body/AdminTableBodyForm.tsx
+++ src/components/Member/AdminList/PageItems/Table/Body/AdminTableBodyForm.tsx
@@ -1,104 +1,140 @@
 import React from "react";
-import { AdminTableBody, AdminTableBodyRowSpan, AdminTableBodyButton } from "./AdminTableBodyForm.styled";
+import {
+  AdminTableBody,
+  AdminTableBodyRowSpan,
+  AdminTableBodyButton,
+  AdminTableProfileImg,
+} from "./AdminTableBodyForm.styled";
 
 import { useAppDispatch, useAppSelector } from "store/rootReducer";
 
 import { AdminTableProps } from "../AdminTableForm";
 import AdminModifyModalForm from "../../AdminModifyModal/AdminModifyModal";
 
-import { banAdminHandler, unbanAdminHandler, adminPasswordInitHandler } from "utils/api/Member/AdminAPI";
+import {
+  banAdminHandler,
+  unbanAdminHandler,
+  adminPasswordInitHandler,
+} from "utils/api/Member/AdminAPI";
 import { AdminAction } from "redux/modules/AdminSlice";
 import { AdminListType } from "types/AdminList";
+import DefaultIcon from "../../../../../../assets/default_user_icon.svg";
 
-const AdminTableBodyForm = ({ offset, postsPerPage, totalContentsCount }: AdminTableProps) => {
-    const { list } = useAppSelector((state) => state.AdminSlice);
-    const dispatch = useAppDispatch();
+const AdminTableBodyForm = ({}: AdminTableProps) => {
+  const { list } = useAppSelector((state) => state.AdminSlice);
+  const dispatch = useAppDispatch();
 
-    const blockButtonOnClickHandler = (admSeq: number) => {
-        banAdminHandler(admSeq)
-            .then((response) => {
-                if (response?.status === 200) {
-                    let newList: AdminListType[] = [];
-                    list.forEach((el) => {
-                        if (el.admSeq === admSeq) {
-                            newList.push({ ...el, isBlock: true });
-                        } else {
-                            newList.push(el);
-                        }
-                    });
-                    dispatch(AdminAction.setAdminList({ list: newList }));
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
-
-    const unblockButtonOnClickHandler = (admSeq: number) => {
-        unbanAdminHandler(admSeq)
-            .then((response) => {
-                if (response?.status === 200) {
-                    let newList: AdminListType[] = [];
-                    list.forEach((el) => {
-                        if (el.admSeq === admSeq) {
-                            newList.push({ ...el, isBlock: false });
-                        } else {
-                            newList.push(el);
-                        }
-                    });
-                    dispatch(AdminAction.setAdminList({ list: newList }));
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
-
-    const passwordInitOnClickHandler = (admSeq: number) => {
-        if (window.confirm("비밀번호를 초기화하시겠습니까?")) {
-            adminPasswordInitHandler(admSeq)
-                .then((response) => {
-                    if (response?.status === 200) {
-                        window.alert("비밀번호 초기화 성공");
-                    }
-                })
-                .catch((error) => {
-                    console.log(error);
-                    window.alert("비밀번호 초기화 실패");
-                });
+  const blockButtonOnClickHandler = (admSeq: number) => {
+    banAdminHandler(admSeq)
+      .then((response) => {
+        if (response?.status === 200) {
+          let newList: AdminListType[] = [];
+          list.forEach((el) => {
+            if (el.admSeq === admSeq) {
+              newList.push({ ...el, isBlock: true });
+            } else {
+              newList.push(el);
+            }
+          });
+          dispatch(AdminAction.setAdminList({ list: newList }));
         }
-    };
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <AdminTableBody key={`uuid`}>
-            {list.slice(offset, offset + postsPerPage).map((admin, index) => {
-                return (
-                    <React.Fragment key={admin.admSeq}>
-                        <tr>
-                            <AdminTableBodyRowSpan>{admin.admProfileImageUrl}</AdminTableBodyRowSpan>
-                            <AdminTableBodyRowSpan>{admin.nickName}</AdminTableBodyRowSpan>
-                            <AdminTableBodyRowSpan>{admin.admEmail}</AdminTableBodyRowSpan>
-                            <AdminTableBodyRowSpan>{admin.admPhone}</AdminTableBodyRowSpan>
-                            <AdminTableBodyRowSpan>{admin.regMagazineCount}</AdminTableBodyRowSpan>
-                            <AdminTableBodyRowSpan>
-                                <AdminModifyModalForm admSeq={admin.admSeq} admName={admin.admName} admNickname={admin.nickName} admEmail={admin.admEmail} admPhone={admin.admPhone} />
-                            </AdminTableBodyRowSpan>
-                            <AdminTableBodyRowSpan>
-                                {admin.isBlock ? (
-                                    <AdminTableBodyButton onClick={() => unblockButtonOnClickHandler(admin.admSeq)}>차단 해제</AdminTableBodyButton>
-                                ) : (
-                                    <AdminTableBodyButton onClick={() => blockButtonOnClickHandler(admin.admSeq)}>차단하기</AdminTableBodyButton>
-                                )}
-                            </AdminTableBodyRowSpan>
-                            <AdminTableBodyRowSpan>
-                                <AdminTableBodyButton onClick={() => passwordInitOnClickHandler(admin.admSeq)}>비밀번호 초기화</AdminTableBodyButton>
-                            </AdminTableBodyRowSpan>
-                        </tr>
-                    </React.Fragment>
-                );
-            })}
-        </AdminTableBody>
-    );
+  const unblockButtonOnClickHandler = (admSeq: number) => {
+    unbanAdminHandler(admSeq)
+      .then((response) => {
+        if (response?.status === 200) {
+          let newList: AdminListType[] = [];
+          list.forEach((el) => {
+            if (el.admSeq === admSeq) {
+              newList.push({ ...el, isBlock: false });
+            } else {
+              newList.push(el);
+            }
+          });
+          dispatch(AdminAction.setAdminList({ list: newList }));
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const passwordInitOnClickHandler = (admSeq: number) => {
+    if (window.confirm("비밀번호를 초기화하시겠습니까?")) {
+      adminPasswordInitHandler(admSeq)
+        .then((response) => {
+          if (response?.status === 200) {
+            window.alert("비밀번호 초기화 성공");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          window.alert("비밀번호 초기화 실패");
+        });
+    }
+  };
+
+  return (
+    <AdminTableBody key={`uuid`}>
+      {list.map((admin, index) => {
+        return (
+          <React.Fragment key={admin.admSeq}>
+            <tr>
+              <AdminTableProfileImg
+                src={
+                  `${admin.admProfileImageUrl}` !== "null"
+                    ? (`${admin.admProfileImageUrl}` as string)
+                    : DefaultIcon
+                }
+              />
+              <AdminTableBodyRowSpan>{admin.nickName}</AdminTableBodyRowSpan>
+              <AdminTableBodyRowSpan>{admin.admEmail}</AdminTableBodyRowSpan>
+              <AdminTableBodyRowSpan>{admin.admPhone}</AdminTableBodyRowSpan>
+              <AdminTableBodyRowSpan>
+                {admin.regMagazineCount}
+              </AdminTableBodyRowSpan>
+              <AdminTableBodyRowSpan>
+                <AdminModifyModalForm
+                  admSeq={admin.admSeq}
+                  admName={admin.admName}
+                  admNickname={admin.nickName}
+                  admEmail={admin.admEmail}
+                  admPhone={admin.admPhone}
+                />
+              </AdminTableBodyRowSpan>
+              <AdminTableBodyRowSpan>
+                {admin.isBlock ? (
+                  <AdminTableBodyButton
+                    onClick={() => unblockButtonOnClickHandler(admin.admSeq)}
+                  >
+                    차단 해제
+                  </AdminTableBodyButton>
+                ) : (
+                  <AdminTableBodyButton
+                    onClick={() => blockButtonOnClickHandler(admin.admSeq)}
+                  >
+                    차단하기
+                  </AdminTableBodyButton>
+                )}
+              </AdminTableBodyRowSpan>
+              <AdminTableBodyRowSpan>
+                <AdminTableBodyButton
+                  onClick={() => passwordInitOnClickHandler(admin.admSeq)}
+                >
+                  비밀번호 초기화
+                </AdminTableBodyButton>
+              </AdminTableBodyRowSpan>
+            </tr>
+          </React.Fragment>
+        );
+      })}
+    </AdminTableBody>
+  );
 };
 
 export default AdminTableBodyForm;
